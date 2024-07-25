0705fd4b1dfd14c6380508181ac73264a688ac8c
SapreGeeta <gitasapre@gmail.com>
2024-07-25 12:08:14 +0530

    login and signup api added

--- server/index.js
+++ server/index.js
@@ -24,6 +24,69 @@ const connectMongoDB = async () => {
 connectMongoDB();
 
 
+
+//Post/login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Please provide email and password",
+    });
+  }
+
+  const user = await User.findOne({
+    email: email,
+    password: password,
+  });
+
+  if (user) {
+    return res.json({
+      success: true,
+      data: user,
+      message: "Login successful",
+    });
+  } else {
+    return res.json({
+      success: false,
+      message: "Invalid credentials",
+    });
+  }
+});
+
+//Post/signup
+app.post("/signup", async (req, res) => {
+  const { name, email, password, mobile, address, gender } = req.body;
+
+  //instance
+  const user = new User({
+    name: name,
+    email: email,
+    password: password,
+    mobile: mobile,
+    address: address,
+    gender: gender,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.json({
+      success: true,
+      data: savedUser,
+      message: "SignUp Successful",
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+
+
 //port
 const PORT = process.env.PORT || 5000;
 
