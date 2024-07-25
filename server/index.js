30af4bceaeeba5052b0b325f8e4c9bb882b9c1b9
SapreGeeta <gitasapre@gmail.com>
2024-07-25 12:14:07 +0530

    product api is added

--- server/index.js
+++ server/index.js
@@ -85,6 +85,131 @@ app.post("/signup", async (req, res) => {
 });
 
 
+//GET  - get/products
+app.get("/products", async (req, res) => {
+  const allproducts = await Product.find();
+
+  res.json({
+    success: true,
+    data: allproducts,
+    message: "All products get successfully",
+  });
+});
+
+//POST - post/product
+
+app.post("/product", async (req, res) => {
+  const { name, description, price, image, category, brand } = req.body;
+
+  //instance
+  const newproduct = new Product({
+    name: name,
+    description: description,
+    price: price,
+    image: image,
+    category: category,
+    brand: brand,
+  });
+
+  try {
+    const saveproduct = await newproduct.save();
+
+    res.json({
+      success: true,
+      data: saveproduct,
+      message: "Product created successfuly",
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      error: err,
+    });
+  }
+});
+
+//GET - get/product/:id
+
+app.get("/product/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const findProduct = await Product.findById({ _id: id });
+
+  res.json({
+    success: true,
+    data: findProduct,
+    message: "Product find successful using ID.",
+  });
+});
+
+//DELETE - delete/product/:id
+
+app.delete("/product/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.deleteOne({ _id: id });
+
+  res.json({
+    success: true,
+    message: "Product deleted successfuly",
+  });
+});
+
+// //PUT - put/product/:id
+
+app.put("/product/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const { name, price, description, category, quantity, image, brand } =
+    req.body;
+
+  await Product.updateOne(
+    { _id: id },
+    {
+      $set: {
+        name,
+        price,
+        description,
+        category,
+        quantity,
+        image,
+        brand,
+      },
+    }
+  );
+
+  try {
+    const findProduct = await Product.findOne({ _id: id });
+    res.json({
+      success: true,
+      data: findProduct,
+      message: "Product updated successfully.",
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: "Not find updated product.",
+    });
+  }
+});
+
+//GET - get/product/search/query
+
+app.get("/search", async (req, res) => {
+  const { q } = req.query;
+
+  const productSearch = await Product.find({
+    name: { $regex: q, $options: "i" },
+  });
+
+  res.json({
+    success: true,
+    data: productSearch,
+    message: "Product fetch Successfully",
+  });
+});
+
+
+
+
 
 
 //port
