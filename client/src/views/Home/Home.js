4522741c018a3c238dd8fdccfc5030907f057903
SapreGeeta <gitasapre@gmail.com>
2024-07-28 14:14:56 +0530

    home function added

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -5,9 +5,41 @@ import axios from 'axios';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import './Home.css'
 
+
+
+function Home() {
+    const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState('');
+  
+    const searchProducts = async() =>{
+      if(search === ''){
+        loadData();
+        return;
+      }
+  
+      const response = await axios.get(`/search?q=${search}`);
+      setProducts(response?.data?.data);
+    }
+  
+    useEffect(()=>{
+      searchProducts();
+    },[search]);
+  
+    const loadData = async ()=>{
+      const response = await axios.get('/products');
+       console.log(response?.data?.data);
+    setProducts(response?.data?.data)
+  }
+  
+  useEffect (()=>{
+    loadData();
+  },[]);
+  
+
+  
 return (
     <div>
-        <Navbar/>
+      
 <div>
   <input type='text'
   className='form-control w-25 mx-auto mt-5'
